0bf62e658e549be5cbb406fdba2c6ddcb6ba657b
Ester Borges de Carvalho <esterborgesdecarvalho@gmail.com>
2023-05-12 23:40:58 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    OK = True
+    for i in range(len(instance)):
+        search_data = instance.search(i)
+        print(search_data)
+        if search_data["nome_do_arquivo"] == path_file:
+            OK = False
+    lines = txt_importer(path_file)
+    if OK:
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+        instance.enqueue(data)
+        print(data)
 
 
 def remove(instance):
